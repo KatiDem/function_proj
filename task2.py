94bc5d197a1d268baa55eeae6526278358ad9942
Katerina <you@a.com>
2021-01-27 20:47:54 +0300

    Add user input

--- task2.py
+++ task2.py
@@ -1,9 +1,10 @@
 def factorial(n):
+    n = int(input('n = '))
     fact = 1
     while n > 0:
         fact *= n
         n -= 1
+    print(f'Factorial is {fact}')
     return fact
 
-
-print(factorial(4))
+factorial(1)
