d21593139565166559c5232c9897ee3821459d87
Katerina <you@a.com>
2021-01-27 21:37:35 +0300

    add file

---
+++ task4.py
@@ -0,0 +1,14 @@
+def sorting(*args):
+    summ = 0
+    n = 0
+    maxim = 0
+    for i in args:
+        summ += i
+        if i > args[-1] and i > maxim:
+            maxim = i
+        n += 1
+    return summ, maxim
+
+
+summ, maxim = sorting(1, 45, 6, 2, 5, 3)
+print(maxim)
